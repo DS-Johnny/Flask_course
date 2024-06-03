d65872d7db75bf81981d4ffb1f17aabd94b653b9
Johnny Matos <128853698+DS-Johnny@users.noreply.github.com>
2024-06-03 15:06:53 -0300

    Criado banco de dados, adicionado templates e arquivos css e bootstrap

--- food_tracker/app.py
+++ food_tracker/app.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('home.html')
 
 @app.route('/view')
 def view():
-    return None
+    return render_template('day.html')
 
 @app.route('/food')
 def food():
-    return None
+    return render_template('add_food.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
