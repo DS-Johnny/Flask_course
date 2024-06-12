7f13896c15502f9750e673487d813066f1d76aff
Johnny Matos <128853698+DS-Johnny@users.noreply.github.com>
2024-06-12 15:16:05 -0300

    New Project - MEMBER API, created directory and app.py

---
+++ member_api/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/member', methods=['GET'])
+def get_members():
+    return 'this returns all the members'
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    return 'this returns one member by ID'
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    return 'This adds a new member.'
+
+@app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
+def edit_member(member_id):
+    return 'This updates a member by ID.'
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    return 'This removes a member by ID.'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
